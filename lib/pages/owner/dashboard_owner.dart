01abf5ef55f2a4c94b72fc829946dfac0c4fa33f
Kevin Lidan <lidannn012@gmail.com>
2024-11-15 19:38:29 +0700

    Merancang halaman dashboard_owner

--- lib/pages/owner/dashboard_owner.dart
+++ lib/pages/owner/dashboard_owner.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:project_bansos/services/auth_services.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dashboard Owner',
+      home: DashboardOwner(),
+    );
+  }
+}
+
 class DashboardOwner extends StatefulWidget {
   const DashboardOwner({super.key});
 
@@ -10,13 +24,97 @@ class DashboardOwner extends StatefulWidget {
 
 class _DashboardOwnerState extends State<DashboardOwner> {
   AuthServices authServices = AuthServices();
+
+  final List<Map<String, dynamic>> products = [
+    {'name': 'Produk A', 'stock': 10},
+    {'name': 'Produk B', 'stock': 5},
+    {'name': 'Produk C', 'stock': 8},
+    {'name': 'Produk D', 'stock': 2},
+    {'name': 'Produk E', 'stock': 7},
+    {'name': 'Produk F', 'stock': 3},
+    {'name': 'Produk G', 'stock': 6},
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: TextButton(
-            onPressed: () {
-              authServices.logoutUser();
-            },
-            child: Text('logout')));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dashboard"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.all(8.0),
+              color: const Color.fromARGB(255, 230, 117, 25),
+              child: Row(
+                children: [
+                  Icon(Icons.warning,
+                      color: const Color.fromARGB(255, 195, 4, 4)),
+                  SizedBox(width: 8.0),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Peringatan",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: const Color.fromARGB(255, 0, 0, 0)),
+                      ),
+                      Text("Stok sudah mau habis!"),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 8.0,
+                  mainAxisSpacing: 8.0,
+                ),
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  final product = products[index];
+                  return Container(
+                    padding: EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 140, 131, 131),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          product['name'],
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 8.0),
+                        Text("Sisa Stok: ${product['stock']}"),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            Center(
+              child: TextButton(
+                onPressed: () {
+                  authServices.logoutUser();
+                },
+                child: Text(
+                  'logout',
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
